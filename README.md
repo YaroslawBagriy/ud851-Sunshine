5983014754e39535362e756f778feb851bf5885e
yaroslawbagriy <yaroslawbagriy@gmail.com>
2019-11-05 11:27:03 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# ud851-Sunshine
+Udacity Course Project: Developing Android Apps
+
+This is the Android weather app that I built during the Developing Android Apps course at Udacity.
+
+Developing Android Apps Course: https://www.udacity.com/course/new-android-fundamentals--ud851
---
+++ app/src/main/java/com/android/yaroslawbagriy/ud851_sunshine/MainActivity.java
@@ -0,0 +1,14 @@
+package com.android.yaroslawbagriy.ud851_sunshine;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
